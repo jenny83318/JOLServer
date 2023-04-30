781f09a0cac2cdd8efd4f371aa23611b754c0884
jenny83318 <jenny83318@gmail.com>
2023-04-30 16:30:04 +0800

    update sql document

--- doc/CustomerInfo_mysql_create.sql
+++ doc/CustomerInfo_mysql_create.sql
@@ -15,7 +15,7 @@ CREATE TABLE `jol_customer` (
   UNIQUE KEY `account` (`account`),
   UNIQUE KEY `email` (`email`),
   UNIQUE KEY `token_UNIQUE` (`token`)
-) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_0900_ai_ci
+) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_0900_ai_ci;
 
 CREATE TABLE `jol_product` (
 	`prodId` int(11) NOT NULL AUTO_INCREMENT,
